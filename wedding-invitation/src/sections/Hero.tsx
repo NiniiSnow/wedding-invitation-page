a5b988e996f62f8795fa0a72153badb53bb8b02e
NiniiSnow <ninii.chirgdze@gmail.com>
2026-06-07 16:54:54 +0400

    Modified location coordinates

--- wedding-invitation/src/sections/Hero.tsx
+++ wedding-invitation/src/sections/Hero.tsx
@@ -10,12 +10,14 @@ export function Hero() {
             <div
                 className="bg-center w-full h-96 flex  justify-center text-center relative"
                 style={{ backgroundImage: "url('/sioni.jpeg')" }}>
-            </div>
 
-            <div>
+            </div>
+{/* 
+            <div
+            >
                 <h1 className="text-white text-4xl md:text-5xl font-bold">{t('hero.title')}</h1>
                 <p className="text-white text-lg md:text-xl mt-4">{t('hero.subtitle')}</p>
-            </div>
+            </div> */}
         </section>
     );
 }   
