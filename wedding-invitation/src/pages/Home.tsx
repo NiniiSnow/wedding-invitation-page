9001328dc01aad6e2fd441bac86c34ab84bf4ab7
NiniiSnow <ninii.chirgadze@gmail.com>
2026-08-20 00:47:11 +0400

    Added guest table and filtering logic

--- wedding-invitation/src/pages/Home.tsx
+++ wedding-invitation/src/pages/Home.tsx
@@ -8,7 +8,6 @@ export function Home() {
             <FloatingFishermen />
             <Invitation />
             <Schedule />
-            {/* <GuestList /> */}
             <Countdown />
             <Faq />
             <Location />
